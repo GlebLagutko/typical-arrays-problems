2128fc386e79f7b84876180f9a4f71a5cafb9acf
Gleb Lagutko <gleblagutko@gmail.com>
2020-09-28 19:57:52 +0300

    feat : done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
+exports.min = function min(array) {
 
-exports.min = function min (array) {
-  return 0;
+    return Array.isArray(array) && array.length > 1 ? Math.min(...array) : 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    return Array.isArray(array) && array.length ? Math.max(...array) : 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    return Array.isArray(array) && array.length > 1 ? array.reduce((a, b) => a + b, 0) /  array.length : 0;
 }
